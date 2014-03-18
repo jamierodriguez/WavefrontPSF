2cc9457d6298ba05a92d63151ca777878b1054b8
cpadavis <chris.pa.davis@gmail.com>
2014-03-18 12:50:18 -0700

    woops forgot some new files

---
+++ WavefrontPSF/analytic_moments.py
@@ -0,0 +1,358 @@
+#!/usr/bin/env python
+"""
+File: analytic_moments.py
+Author: Chris Davis
+Description: Class for creating wavefronts analytically from a given zernike.
+"""
+
+from __future__ import print_function, division
+import numpy as np
+import sympy as sp
+
+###############################################################################
+# create the analytic arrays
+###############################################################################
+
+w, R = sp.symbols('w, R')
+ws = sp.conjugate(w)
+
+# put in the gradient by hand
+# TODO: double check 0th components
+# TODO: also double check effects of conjugation
+#       this extends to the 2 and 3 components
+grad = [0,
+        0,
+        0,
+        0,
+        (4 * np.sqrt(3) * w),
+        (2 * np.sqrt(6) * sp.I * ws),
+        (2 * np.sqrt(6) * ws),
+        (-sp.I * np.sqrt(8) * (3 * w ** 2 - 6 * w * ws + 2)),
+        (np.sqrt(8) * (3 * w ** 2 + 6 * w * ws - 2)),
+        (sp.I * 3 * np.sqrt(8) * ws ** 2),
+        (3 * np.sqrt(8) * ws ** 2),
+        24 * np.sqrt(5) * w * (w * ws - 1/2)]
+
+estimate = {}
+z_list = sp.symbols('z_0:12', real = True)
+expression = {}
+
+# look at e0 term
+real = []
+imaginary = []
+expr_re = 0
+expr_im = 0
+for i in range(1, 12):
+    for j in range(1, 12):
+        expr = grad[i] * sp.conjugate(grad[j])
+        expr = sp.expand(expr)
+        expr = expr.subs(w * sp.conjugate(w), R)
+
+        # now replace any other w or sp.conjugate(w) with 0
+        expr = expr.subs(w, 0)
+
+        # now replace R^4 with 1/5, etc
+        for rpow in range(8, 0, -1):
+            expr = expr.subs(R ** rpow, 1 / (rpow + 1))
+        if sp.re(expr) != 0:
+            real.append([float(sp.re(expr)), i, j])
+            expr_re = expr_re + z_list[i] * z_list[j] * sp.re(expr)
+        if sp.im(expr) != 0:
+            imaginary.append([float(sp.im(expr)), i, j])
+            expr_im = expr_im + z_list[i] * z_list[j] * sp.im(expr)
+real = np.array(real)
+imaginary = np.array(imaginary)
+estimate.update(dict(e0 = real))
+expression.update(dict(e0 = expr_re))
+
+
+# look at e term
+real = []
+imaginary = []
+expr_re = 0
+expr_im = 0
+for i in range(1, 12):
+    for j in range(1, 12):
+        expr = grad[i] * grad[j]
+        expr = sp.expand(expr)
+        expr = expr.subs(w * sp.conjugate(w), R)
+
+        # now replace any other w or sp.conjugate(w) with 0
+        expr = expr.subs(w, 0)
+
+        # now replace R^4 with 1/5, etc
+        for rpow in range(8, 0, -1):
+            expr = expr.subs(R ** rpow, 1 / (rpow + 1))
+        if sp.re(expr) != 0:
+            real.append([float(sp.re(expr)), i, j])
+            expr_re = expr_re + z_list[i] * z_list[j] * sp.re(expr)
+        if sp.im(expr) != 0:
+            imaginary.append([float(sp.im(expr)), i, j])
+            expr_im = expr_im + z_list[i] * z_list[j] * sp.im(expr)
+real = np.array(real)
+imaginary = np.array(imaginary)
+estimate.update(dict(e1 = real, e2 = imaginary))
+expression.update(dict(e1 = expr_re, e2 = expr_im))
+
+# look at delta term
+real = []
+imaginary = []
+expr_re = 0
+expr_im = 0
+for i in range(1, 12):
+    for j in range(1, 12):
+        for k in range(1, 12):
+            expr = grad[i] * grad[j] * grad[k]
+            expr = sp.expand(expr)
+            expr = expr.subs(w * sp.conjugate(w), R)
+
+            # now replace any other w or sp.conjugate(w) with 0
+            expr = expr.subs(w, 0)
+
+            # now replace R^4 with 1/5, etc
+            for rpow in range(8, 0, -1):
+                expr = expr.subs(R ** rpow, 1 / (rpow + 1))
+            if sp.re(expr) != 0:
+                real.append([float(sp.re(expr)), i, j, k])
+                expr_re = expr_re + z_list[i] * z_list[j] * z_list[k] * \
+                        sp.re(expr)
+
+            if sp.im(expr) != 0:
+                imaginary.append([float(sp.im(expr)), i, j, k])
+                expr_im = expr_re + z_list[i] * z_list[j] * z_list[k] * \
+                        sp.im(expr)
+real = np.array(real)
+imaginary = np.array(imaginary)
+estimate.update(dict(delta1 = real, delta2 = imaginary))
+expression.update(dict(delta1 = expr_re, delta2 = expr_im))
+
+# look at zeta term
+real = []
+imaginary = []
+expr_re = 0
+expr_im = 0
+for i in range(1, 12):
+    for j in range(1, 12):
+        for k in range(1, 12):
+            expr = grad[i] * grad[j] * sp.conjugate(grad[k])
+            expr = sp.expand(expr)
+            expr = expr.subs(w * sp.conjugate(w), R)
+
+            # now replace any other w or sp.conjugate(w) with 0
+            expr = expr.subs(w, 0)
+
+            # now replace R^4 with 1/5, etc
+            for rpow in range(8, 0, -1):
+                expr = expr.subs(R ** rpow, 1 / (rpow + 1))
+            if sp.re(expr) != 0:
+                real.append([float(sp.re(expr)), i, j, k])
+                expr_re = expr_re + z_list[i] * z_list[j] * z_list[k] * \
+                        sp.re(expr)
+            if sp.im(expr) != 0:
+                imaginary.append([float(sp.im(expr)), i, j, k])
+                expr_im = expr_re + z_list[i] * z_list[j] * z_list[k] * \
+                        sp.im(expr)
+real = np.array(real)
+imaginary = np.array(imaginary)
+estimate.update(dict(zeta1 = real, zeta2 = imaginary))
+expression.update(dict(zeta1 = expr_re, zeta2 = expr_im))
+
+# construct derivative
+derivative = {}
+for key in estimate.keys():
+    derivative_key = []
+    for zi in range(12):
+        derivative_i = []
+        # choose only the ones with zi in it
+        chosen = np.any(estimate[key][:, 1:] == zi, axis=1)
+        # now create an entry for each zi
+        temp = estimate[key][chosen].tolist()
+        # go through each entry in temp
+        for temp_i in temp:
+            for ij in xrange(1, len(temp_i)):
+                temp_ij = temp_i[ij]
+                if temp_ij == zi:
+                    # append the temp list sans this item
+                    derivative_i.append(temp_i[:ij] + temp_i[ij + 1:])
+        derivative_key.append(np.array(derivative_i))
+
+    derivative.update({key: derivative_key})
+
+
+# now we also need to include the scalings with rzero
+# [oneover?, slope, intercept]
+rzero_scaling = {'e0': {'slope': [1.0601e-04, 3.2515e-03, -4.6234e-06],
+                        'intercept': [9.3828e-04, 6.5692e-03, 4.2024e-03],},
+                 'e1': {'slope': [8.7110e-05, 2.7806e-03, -3.5211e-06],
+                        'intercept': [1.6411e-05, 3.8464e-04, 6.5707e-07],},
+                 'e2': {'slope': [1.0975e-04, 2.7569e-03, -3.3518e-06],
+                        'intercept': [3.6041e-05, -3.7639e-04, -6.8314e-07],},
+                 'delta1': {'slope': [-7.7465e-06, -3.8174e-05, 7.8885e-08],
+                        'intercept': [-3.1853e-07, 7.0394e-07, 7.4074e-08],},
+                 'delta2': {'slope': [-1.0437e-05, -3.8929e-05, 1.7061e-07],
+                        'intercept': [-5.5116e-07, 5.7043e-06, 8.2532e-08],},
+                 'zeta1': {'slope': [-1.6626e-06, -3.4517e-05, 2.4426e-08],
+                        'intercept': [-3.3082e-07, -2.8350e-05, 1.2584e-07],},
+                 'zeta2': {'slope': [-1.3047e-06, -3.7087e-05, 3.8996e-09],
+                        'intercept': [7.5179e-07, -2.7431e-05, 1.6107e-08],},
+                 }
+
+def analytic_data(zernikes, rzero, coords=[]):
+    return_dict = {}
+
+    for key in estimate.keys():
+        # TODO: should be possible to do this numpy-like?
+        # Do estimate
+        val = 0
+        for term in estimate[key]:
+            val_i = term[0]
+            for term_i in xrange(1, len(term)):
+                if term[term_i] <= zernikes.shape[1]:
+                    val_i *= zernikes[:, term[term_i] - 1]
+                else:
+                    val_i *= 0
+            val += val_i
+
+        # incorporate rzero slope and intercept
+        rzero_scaling_items = rzero_scaling[key]['slope']
+
+        slope = rzero_scaling_items[0] / rzero + \
+                rzero_scaling_items[1] + \
+                rzero_scaling_items[2] / rzero ** 2
+
+
+        rzero_scaling_items = rzero_scaling[key]['intercept']
+
+        intercept = rzero_scaling_items[0] / rzero + \
+                    rzero_scaling_items[1] + \
+                    rzero_scaling_items[2] / rzero ** 2
+
+
+
+        val = val * slope + intercept
+
+        return_dict.update({key: val})
+
+    if len(coords) > 0:
+        return_dict.update({'x': coords[:,0],
+                            'y': coords[:,1],
+                            'chip': coords[:,2]})
+
+    return return_dict
+
+def analytic_derivative_data(zernikes, rzero, coords=[]):
+    return_dict = {}
+    for key in derivative.keys():
+        return_dict.update({key: {}})
+        for zi in range(12):
+            val = 0
+            for term in derivative[key][zi]:
+                if len(term) > 0:
+                    val_i = term[0]
+                    for term_i in xrange(1, len(term)):
+                        if term[term_i] <= zernikes.shape[1]:
+                            val_i *= zernikes[:, term[term_i] - 1]
+                        else:
+                            val_i *= 0
+                    val += val_i
+
+            # incorporate rzero slope. No intercept for derivative
+            rzero_scaling_items = rzero_scaling[key]['slope']
+
+            slope = rzero_scaling_items[0] / rzero + \
+                    rzero_scaling_items[1] + \
+                    rzero_scaling_items[2] / rzero ** 2
+
+            val = val * slope
+
+            return_dict[key].update({'z{0:02d}'.format(zi): val})
+
+        # throw in rzero derivative
+        val = 0
+        for term in estimate[key]:
+            val_i = term[0]
+            for term_i in xrange(1, len(term)):
+                if term[term_i] <= zernikes.shape[1]:
+                    val_i *= zernikes[:, term[term_i] - 1]
+                else:
+                    val_i *= 0
+            val += val_i
+
+        rzero_scaling_items = rzero_scaling[key]['slope']
+
+        slope = -rzero_scaling_items[0] / rzero ** 2 + \
+                -2 * rzero_scaling_items[2] / rzero ** 3
+
+
+        rzero_scaling_items = rzero_scaling[key]['intercept']
+
+        intercept = - rzero_scaling_items[0] / rzero ** 2 + \
+                    -2 * rzero_scaling_items[2] / rzero ** 3
+
+        val = val * slope + intercept
+
+        return_dict[key].update({'rzero': val})
+
+
+    if len(coords) > 0:
+        return_dict.update({'x': coords[:,0],
+                            'y': coords[:,1],
+                            'chip': coords[:,2]})
+
+    return return_dict
+
+def get_derivative(key, zi):
+    return get_expression(key).diff(z_list[zi])
+
+def get_expression(key):
+    # give sympy expression
+
+    estimate_array = estimate[key]
+    expr = 0
+    for term in estimate_array:
+        val_i = term[0]
+        for term_i in xrange(1, len(term)):
+            val_i = val_i * z_list[np.int(term[term_i])]
+        expr = expr + val_i
+    return expr
+
+def print_expression(key):
+    # same idea but list of terms
+
+    estimate_array = estimate[key]
+    vals = []
+    zs = []
+    for term in estimate_array:
+        val = term[0]
+        zs_i = sorted(list(term[1:]))
+
+        # find if permutation of zs is already in expr
+        same = False
+        same_i = 0
+        while (not same) * (same_i < len(zs)):
+            zs_same = sorted(zs[same_i])
+            same = (zs_same == zs_i)
+            same_i += 1
+        # if so, add val to it!
+        if same:
+            vals[same_i - 1] += val
+        else:
+            # if not create a new one!
+            zs.append(zs_i)
+            vals.append(val)
+
+    # sort them; this is pretty fancy (thanks stackoverflow!)
+    # http://stackoverflow.com/questions/9543211/sorting-a-list-in-python-using-the-result-from-sorting-another-list
+    zs, vals = zip(*sorted(zip(zs, vals), key=lambda x: sorted(x[0])))
+
+    # now construct strings for printing this
+    string_expression = []
+    total_expression = ''
+    for i in xrange(len(vals)):
+        value = '{0:.2f} '.format(vals[i])
+        for zi in zs[i]:
+            value += 'z_{' + '{0}'.format(int(zi)) + '} '
+        string_expression.append(value)
+        total_expression += value + '+ '
+    string_expression.append(total_expression[:-3])
+    return string_expression
---
+++ batch/fit_analytic.py
@@ -0,0 +1,387 @@
+#!/usr/bin/env python
+"""
+File: fit_analytic.py
+Author: Chris Davis
+Description: File that takes a DES catalog and fits via analytic function
+"""
+
+from __future__ import print_function, division
+import matplotlib
+# the agg is so I can submit for batch jobs.
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+from focal_plane import FocalPlane
+from focal_plane_fit import FocalPlaneFit
+from colors import blue_red, blues_r, reds
+from minuit_fit import Minuit_Fit
+from routines import minuit_dictionary, mean_trim, vary_one_parameter
+from routines_moments import convert_moments
+from routines_files import generate_hdu_lists, make_directory
+from routines_plot import data_focal_plot, data_hist_plot, save_func_hists
+
+##############################################################################
+# argparse
+##############################################################################
+
+parser = argparse. \
+    ArgumentParser(description=
+                   'Fit image and dump results.')
+parser.add_argument("-e",
+                    dest="expid",
+                    type=int,
+                    help="what image number will we fit now?")
+parser.add_argument("-b",
+                    dest="boxdiv",
+                    type=int,
+                    default=1,
+                    help="divisions of the box we care about")
+parser.add_argument("-n",
+                    dest="methodVal",
+                    type=int,
+                    default=20,
+                    help="number of nearest neighbors used for reference mesh")
+parser.add_argument("-v",
+                    dest="verbose",
+                    type=int,
+                    default=1,
+                    help="if > 0, print verbose statements")
+parser.add_argument("-s",
+                    dest="n_samples_box",
+                    type=int,
+                    default=10,
+                    help="how many entries per box are we pulling")
+parser.add_argument("-i",
+                    dest="save_iter",
+                    type=int,
+                    default=10,
+                    help="how often to save reference plot")
+parser.add_argument("-o",
+                    dest="output_directory",
+                    default='/nfs/slac/g/ki/ki18/cpd/focus/outputs/',
+                    help="where will the outputs go")
+parser.add_argument("-t",
+                    dest="catalogs",
+                    default='/nfs/slac/g/ki/ki18/cpd/catalogs/wgetscript/',
+                    help='directory containing the catalogs')
+options = parser.parse_args()
+
+args_dict = vars(options)
+
+verbose = args_dict['verbose']
+
+average = np.mean  # TODO: this may change!!!
+boxdiv = args_dict['boxdiv']
+conds = 'default'
+max_samples_box = 100000
+n_samples_box = args_dict['n_samples_box']
+subav = False
+methodVal = (args_dict['methodVal'], 1.)
+
+expid = args_dict['expid']
+dataDirectory = args_dict['catalogs']
+list_catalogs, list_fits_extension, list_chip = \
+        generate_hdu_lists(expid,
+                           path_base=dataDirectory)
+
+output_directory = args_dict['output_directory']
+make_directory(output_directory)
+
+##############################################################################
+# set up fit
+##############################################################################
+
+chi_weights = {
+    'e0': 0.01,
+    'e1': 1.,
+    'e2': 1.,
+    'delta1': 0.1,
+    'delta2': 0.1,
+    'zeta1': 0.01,
+    'zeta2': 0.01,
+    }
+
+FP = FocalPlane(list_catalogs=list_catalogs,
+                list_fits_extension=list_fits_extension,
+                list_chip=list_chip,
+                boxdiv=boxdiv,
+                max_samples_box=max_samples_box,
+                conds=conds,
+                average=average,
+                subav=subav,
+                )
+edges = FP.decaminfo.getEdges(boxdiv=boxdiv)
+
+data_compare = FP.data
+data_unaveraged_compare = FP.data_unaveraged
+edges = FP.decaminfo.getEdges(boxdiv=boxdiv)
+
+# create subsample
+recdata_sample, extension_sample = FP.filter_number_in_box(
+        recdata=FP.recdata,
+        extension=FP.extension,
+        max_samples_box=n_samples_box,
+        boxdiv=boxdiv)
+data_sample, coords_sample, data_unaveraged_sample = FP.create_data(
+    recdata=recdata_sample,
+    extension=extension_sample,
+    average=average,
+    boxdiv=boxdiv,
+    subav=False,
+    )
+
+
+# creat FocalPlaneFit object
+FPF = FocalPlaneFit(methodVal=methodVal)
+
+# give FPF some attributes
+FPF.chi_weights = chi_weights
+FPF.coords = coords_sample
+
+# define the fit function
+chi2hist = []
+FPF.history = []
+
+plane_func = FPF.analytic_plane_averaged
+#plane_func = FPF.plane_averaged
+
+def FitFunc(in_dict):
+
+
+    # go through the key_FP_funcs and make sure there are no nans
+    for key_FP_func in in_dict.keys():
+        if np.isnan(in_dict[key_FP_func]).any():
+            # if there is a nan, don't even bother calling, just return a
+            # big chi2
+            FPF.remakedonut()
+            return 1e20
+
+    poles_i = plane_func(in_dict, coords=coords_sample,
+                         average=average, boxdiv=boxdiv,
+                         subav=subav)
+
+    FPF.temp_plane = poles_i
+
+    chi2 = FPF.compare(poles_i, data_compare, var_dict=data_compare,
+                       chi_weights=FPF.chi_weights)
+    chi2hist.append(chi2)
+
+    return chi2
+
+# define the save function
+def SaveFunc(steps, defaults=False):
+    in_dict = {'steps': steps,
+               'state_history': FPF.history,
+               'chisquared_history': chi2hist,
+               'chi_weights': FPF.chi_weights,
+               'plane': FPF.temp_plane,
+               'reference_plane': data_compare,
+               'output_directory': output_directory,
+               'boxdiv': boxdiv,
+               'edges': edges,
+               'defaults': defaults,}
+    save_func_hists(**in_dict)
+
+    return
+
+# fit
+p_init = {
+    'delta1' : 0,
+    'delta2' : 0,
+##     'dx' :     0,
+##     'dy' :     0,
+##     'dz' :     0,
+    'e1' :     0,
+    'e2' :     0,
+    'rzero' :  0.14,
+##     'xt' :     0,
+##     'yt' :     0,
+    'z04d' :   0,
+##     'z04x' :   0,
+##     'z04y' :   0,
+    'z05d' :   0,
+    'z05x' :   0,
+    'z05y' :   0,
+    'z06d' :   0,
+    'z06x' :   0,
+    'z06y' :   0,
+    'z07d' :   0,
+##     'z07x' :   0,
+##     'z07y' :   0,
+    'z08d' :   0,
+##     'z08x' :   0,
+##     'z08y' :   0,
+    'z09d' :   0,
+##     'z09x' :   0,
+##     'z09y' :   0,
+    'z10d' :   0,
+##     'z10x' :   0,
+##     'z10y' :   0,
+    'z11d' :   0,
+##     'z11x' :   0,
+##     'z11y' :   0,
+    'zeta1' :  0,
+    'zeta2' :  0,
+    }
+
+par_names = sorted(p_init.keys())
+h_base = 1e-3
+# set up initial guesses
+minuit_dict, h_dict = minuit_dictionary(par_names, h_base=h_base)
+for pkey in p_init:
+    minuit_dict[pkey] = p_init[pkey]
+
+# setup object
+verbosity = 3
+force_derivatives = 1
+strategy = 1
+tolerance = 1
+save_iter = args_dict['save_iter']
+max_iterations = len(par_names) * 100
+
+minuit_fit = Minuit_Fit(FitFunc, minuit_dict, par_names=par_names,
+                        SaveFunc=SaveFunc,
+                        save_iter=save_iter,
+                        h_dict=h_dict,
+                        verbosity=verbosity,
+                        force_derivatives=force_derivatives,
+                        strategy=strategy, tolerance=tolerance,
+                        max_iterations=max_iterations)
+
+##############################################################################
+# do fit
+##############################################################################
+
+minuit_fit.setupFit()
+minuit_fit.doFit()
+
+##############################################################################
+# assess how well we did
+##############################################################################
+
+minuit_results = minuit_fit.outFit()
+nCalls = (int(minuit_fit.nCalls / 1000) + 1) * 1000
+
+# append to minuit results the argdict
+minuit_results.update({'args_dict': args_dict})
+
+np.save(output_directory + 'minuit_results', minuit_results)
+
+# also save the coordinates
+np.save(output_directory + 'coords_sample', coords_sample)
+
+if verbose:
+    for key in sorted(minuit_results['args']):
+        print(key, p_init[key], minuit_results['args'][key],
+              minuit_results['errors'][key])
+    for key in sorted(minuit_results):
+        if (key == 'correlation') + (key == 'covariance'):
+            continue
+        print(key, minuit_results[key])
+    for key in sorted(minuit_results['args']):
+        print("'" + key + "'", ":", minuit_results['args'][key], ",")
+
+##############################################################################
+# figures
+##############################################################################
+
+if verbose:
+    fig = plt.figure()
+    plt.imshow(minuit_results['correlation'], cmap=blue_red,
+               interpolation='none', vmin=-1, vmax=1,
+               origin='lower')
+    plt.colorbar()
+    fig.savefig(output_directory + '{0:04d}_correlation'.format(nCalls) +
+                '.png')
+    plt.close('all')
+
+    chi2out = FitFunc(minuit_results['args'])
+    SaveFunc(nCalls, defaults=True)
+    # refrence histograms
+    figures, axes, scales = data_hist_plot(data_compare, edges)
+    for fig in figures:
+        figures[fig].savefig(output_directory +
+           '{0:04d}_reference_histograms_{1}.png'.format(nCalls, fig))
+    plt.close('all')
+    # refrence_sample histograms
+    figures, axes, scales = data_hist_plot(data_unaveraged_compare, edges)
+    for fig in figures:
+        figures[fig].savefig(output_directory +
+           '{0:04d}_sampled_histograms_{1}.png'.format(nCalls, fig))
+    plt.close('all')
+    in_dict = minuit_results['args']
+    poles_i = plane_func(in_dict, coords=coords_sample,
+                         average=average, boxdiv=boxdiv,
+                         subav=subav)
+
+    poles_i = convert_moments(poles_i)
+    # final histograms
+    figures, axes, scales = data_hist_plot(poles_i, edges)
+    for fig in figures:
+        figures[fig].savefig(output_directory +
+           '{0:04d}_fit_histograms_{1}.png'.format(nCalls, fig))
+    plt.close('all')
+    # do focal plane plots
+    figures, axes, scales = data_focal_plot(data_compare, color='r',
+                                            defaults=True)
+    figures, axes, scales = data_focal_plot(poles_i, color='b',
+                                            figures=figures, axes=axes,
+                                            scales=scales)
+    for fig in figures:
+        figures[fig].savefig(output_directory +
+           '{0:04d}_focal_plot_{1}.png'.format(nCalls, fig))
+    plt.close('all')
+
+    fig = plt.figure(figsize=(12, 12), dpi=300)
+    for key in sorted(chi2hist[-1].keys()):
+        x = range(len(chi2hist))
+        y = []
+        for i in x:
+            if key == 'chi2':
+                y.append(np.sum(chi2hist[i][key]))
+            else:
+                y.append(np.sum(chi2hist[i][key] * chi_weights[key]))
+        if key == 'chi2':
+            plt.plot(x, np.log10(y), 'k--', label=key)
+        else:
+            plt.plot(x, np.log10(y), label=key)
+    plt.legend()
+    fig.savefig(output_directory + '{0:04d}_chi2hist'.format(nCalls) + '.png')
+    plt.close('all')
+
+    # plot values
+    for key in sorted(FPF.history[-1].keys()):
+        fig = plt.figure(figsize=(12, 12), dpi=300)
+        x = range(len(FPF.history))
+        y = []
+        for i in x:
+            y.append(np.sum(FPF.history[i][key]))
+        plt.plot(x, y, label=key)
+        plt.title('{0}'.format(key))
+        fig.savefig(output_directory + '{0:04d}_{1}_history'.format(nCalls,
+                                                                    key)
+                    + '.png')
+        plt.close('all')
+
+    # do varying parameters
+    for parameter in par_names:
+        chi2, parameters = vary_one_parameter(parameter, FitFunc,
+                                              minuit_results['minuit'])
+        fig = plt.figure(figsize=(12, 12))
+        for key in sorted(chi2[-1].keys()):
+            chi2par = [np.sum(chi2_i[key]) for chi2_i in chi2]
+            if key == 'chi2':
+                plt.semilogy(parameters, chi2par, 'k--', label=key)
+                plt.plot([minuit_results['minuit'][parameter],
+                          minuit_results['minuit'][parameter]],
+                         [np.min(chi2par), np.max(chi2par)], 'k:', linewidth=2)
+            else:
+                plt.semilogy(parameters, chi2par, '-', label=key)
+        plt.legend()
+        plt.title(parameter)
+        fig.savefig(output_directory + '{0:04d}_vary_'.format(nCalls) +
+                    parameter + '.png')
+        plt.close('all')
+
