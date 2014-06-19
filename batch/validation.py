e057c5c8e1a793cce418b5fbd4ef1f0537b6ee7c
cpadavis <chris.pa.davis@gmail.com>
2014-06-19 12:55:00 -0700

    added some options for validation.py

--- batch/validation.py
+++ batch/validation.py
@@ -50,6 +50,33 @@ parser.add_argument("-s",
                     type=int,
                     help="save stamp / vignet parameter?")
 
+parser.add_argument("--training",
+                    dest="training",
+                    default=1,
+                    type=int,
+                    help="Do training ones?")
+parser.add_argument("--psfex",
+                    dest="psfex",
+                    default=1,
+                    type=int,
+                    help="Do PSFex ones?")
+parser.add_argument("--data",
+                    dest="data",
+                    default=1,
+                    type=int,
+                    help="Do data ones?")
+parser.add_argument("--fit",
+                    dest="fit",
+                    default=1,
+                    type=int,
+                    help="Do fit ones?")
+parser.add_argument("--fit_analytic",
+                    dest="fit_analytic",
+                    default=1,
+                    type=int,
+                    help="Do fit_analytic ones?")
+
+
 options = parser.parse_args()
 
 args_dict = vars(options)
@@ -78,19 +105,22 @@ psfex_list_catalogs, psfex_list_fits_extension, psfex_list_chip = \
             expid, path_base=path_base,
             name='valpsfcat',
             extension=2)
-FP = FocalPlane(psfex_list_catalogs, psfex_list_fits_extension,
-                psfex_list_chip,
-                conds='minimal',
-                n_samples_box=10000)
+if options['data']:
+    FP = FocalPlane(psfex_list_catalogs, psfex_list_fits_extension,
+                    psfex_list_chip,
+                    conds='minimal',
+                    n_samples_box=10000)
 
 psfex_list_catalogs_sel = [psfex_list_catalogs_i.replace(
                             'val', 'sel')
                             for psfex_list_catalogs_i in psfex_list_catalogs]
 
-FP_sel = FocalPlane(psfex_list_catalogs_sel, psfex_list_fits_extension,
-                    psfex_list_chip,
-                    conds='minimal',
-                    n_samples_box=10000)
+if options['training']:
+    if options['data']:
+        FP_sel = FocalPlane(psfex_list_catalogs_sel, psfex_list_fits_extension,
+                            psfex_list_chip,
+                            conds='minimal',
+                            n_samples_box=10000)
 
 
 # psfex fit
@@ -99,9 +129,10 @@ psfex_list_catalogs_2 = [psfex_list_catalogs_i.replace(
                                 'valpsfcat', 'psfcat_validation_subtracted')
                        for psfex_list_catalogs_i in psfex_list_catalogs]
 
-FPP = FocalPlanePSFEx(psfex_list_catalogs_2,
-                      np.array(psfex_list_chip).flatten(),
-                      verbosity=verbosity_list)
+if options['psefex']:
+    FPP = FocalPlanePSFEx(psfex_list_catalogs_2,
+                          np.array(psfex_list_chip).flatten(),
+                          verbosity=verbosity_list)
 
 # psfex fit
 psfex_list_catalogs_2_sel = [psfex_list_catalogs_i.replace(
@@ -109,9 +140,11 @@ psfex_list_catalogs_2_sel = [psfex_list_catalogs_i.replace(
                                 'valpsfcat', 'psfcat')
                        for psfex_list_catalogs_i in psfex_list_catalogs]
 
-FPP_sel = FocalPlanePSFEx(psfex_list_catalogs_2_sel,
-                      np.array(psfex_list_chip).flatten(),
-                      verbosity=verbosity_list)
+if options['training']:
+    if options['psfex']:
+        FPP_sel = FocalPlanePSFEx(psfex_list_catalogs_2_sel,
+                              np.array(psfex_list_chip).flatten(),
+                              verbosity=verbosity_list)
 
 
 
@@ -129,85 +162,96 @@ for key in dat.dtype.names:
 # generate catalogs
 ##############################################################################
 
-FP_unaveraged = FP.data_unaveraged
-PSFEx_unaveraged = FPP.plane(FP.coords)
-WavefrontPSF_analytic_unaveraged = FPF.analytic_plane(FPFdict, FP.coords)
-WavefrontPSF_unaveraged = FPF.plane(FPFdict, FP.coords)
+if options['data']:
+    FP_unaveraged = FP.data_unaveraged
+if options['psfex']:
+    PSFEx_unaveraged = FPP.plane(FP.coords)
+if options['fit_analytic']
+    WavefrontPSF_analytic_unaveraged = FPF.analytic_plane(FPFdict, FP.coords)
+if options['fit']:
+    WavefrontPSF_unaveraged = FPF.plane(FPFdict, FP.coords)
 
-FP_unaveraged_sel = FP_sel.data_unaveraged
-PSFEx_unaveraged_sel = FPP.plane(FP_sel.coords)
-WavefrontPSF_analytic_unaveraged_sel = FPF.analytic_plane(FPFdict, FP_sel.coords)
-WavefrontPSF_unaveraged_sel = FPF.plane(FPFdict, FP_sel.coords)
+if options['training']:
+    if options['data']:
+        FP_unaveraged_sel = FP_sel.data_unaveraged
+    if options['psfex']:
+        PSFEx_unaveraged_sel = FPP.plane(FP_sel.coords)
+    if options['fit_analytic']:
+        WavefrontPSF_analytic_unaveraged_sel = FPF.analytic_plane(FPFdict, FP_sel.coords)
+    if options['fit']:
+        WavefrontPSF_unaveraged_sel = FPF.plane(FPFdict, FP_sel.coords)
 
 # filter out stamp parameters
 # TODO: this is cludge
 stamp_names = ['vignet', 'stamp', 'VIGNET', 'STAMP']
 if stamp_save == 0:
     # these are all dicts so remake them
-    FP_unaveraged_fil = {}
-    for key in FP_unaveraged.keys():
-        if key in stamp_names:
-            continue
-        else:
-            FP_unaveraged_fil.update({key: FP_unaveraged[key]})
-    FP_unaveraged = FP_unaveraged_fil
+    if options['data']:
+        FP_unaveraged_fil = {}
+        for key in FP_unaveraged.keys():
+            if key in stamp_names:
+                continue
+            else:
+                FP_unaveraged_fil.update({key: FP_unaveraged[key]})
+        FP_unaveraged = FP_unaveraged_fil
+    if options['psfex']:
+        PSFEx_unaveraged_fil = {}
+        for key in PSFEx_unaveraged.keys():
+            if key in stamp_names:
+                continue
+            else:
+                PSFEx_unaveraged_fil.update({key: PSFEx_unaveraged[key]})
+        PSFEx_unaveraged = PSFEx_unaveraged_fil
+    if options['fit_analytic']:
+        WavefrontPSF_analytic_unaveraged_fil = {}
+        for key in WavefrontPSF_analytic_unaveraged.keys():
+            if key in stamp_names:
+                continue
+            else:
+                WavefrontPSF_analytic_unaveraged_fil.update({key: WavefrontPSF_analytic_unaveraged[key]})
+        WavefrontPSF_analytic_unaveraged = WavefrontPSF_analytic_unaveraged_fil
+    if options['fit']:
+        WavefrontPSF_unaveraged_fil = {}
+        for key in WavefrontPSF_unaveraged.keys():
+            if key in stamp_names:
+                continue
+            else:
+                WavefrontPSF_unaveraged_fil.update({key: WavefrontPSF_unaveraged[key]})
+        WavefrontPSF_unaveraged = WavefrontPSF_unaveraged_fil
 
-    PSFEx_unaveraged_fil = {}
-    for key in PSFEx_unaveraged.keys():
-        if key in stamp_names:
-            continue
-        else:
-            PSFEx_unaveraged_fil.update({key: PSFEx_unaveraged[key]})
-    PSFEx_unaveraged = PSFEx_unaveraged_fil
-
-    WavefrontPSF_analytic_unaveraged_fil = {}
-    for key in WavefrontPSF_analytic_unaveraged.keys():
-        if key in stamp_names:
-            continue
-        else:
-            WavefrontPSF_analytic_unaveraged_fil.update({key: WavefrontPSF_analytic_unaveraged[key]})
-    WavefrontPSF_analytic_unaveraged = WavefrontPSF_analytic_unaveraged_fil
-
-    WavefrontPSF_unaveraged_fil = {}
-    for key in WavefrontPSF_unaveraged.keys():
-        if key in stamp_names:
-            continue
-        else:
-            WavefrontPSF_unaveraged_fil.update({key: WavefrontPSF_unaveraged[key]})
-    WavefrontPSF_unaveraged = WavefrontPSF_unaveraged_fil
-
-
-    FP_unaveraged_sel_fil = {}
-    for key in FP_unaveraged_sel.keys():
-        if key in stamp_names:
-            continue
-        else:
-            FP_unaveraged_sel_fil.update({key: FP_unaveraged_sel[key]})
-    FP_unaveraged_sel = FP_unaveraged_sel_fil
-
-    PSFEx_unaveraged_sel_fil = {}
-    for key in PSFEx_unaveraged_sel.keys():
-        if key in stamp_names:
-            continue
-        else:
-            PSFEx_unaveraged_sel_fil.update({key: PSFEx_unaveraged_sel[key]})
-    PSFEx_unaveraged_sel = PSFEx_unaveraged_sel_fil
-
-    WavefrontPSF_analytic_unaveraged_sel_fil = {}
-    for key in WavefrontPSF_analytic_unaveraged_sel.keys():
-        if key in stamp_names:
-            continue
-        else:
-            WavefrontPSF_analytic_unaveraged_sel_fil.update({key: WavefrontPSF_analytic_unaveraged_sel[key]})
-    WavefrontPSF_analytic_unaveraged_sel = WavefrontPSF_analytic_unaveraged_sel_fil
-
-    WavefrontPSF_unaveraged_sel_fil = {}
-    for key in WavefrontPSF_unaveraged_sel.keys():
-        if key in stamp_names:
-            continue
-        else:
-            WavefrontPSF_unaveraged_sel_fil.update({key: WavefrontPSF_unaveraged_sel[key]})
-    WavefrontPSF_unaveraged_sel = WavefrontPSF_unaveraged_sel_fil
+    if options['training']:
+        if options['data']:
+            FP_unaveraged_sel_fil = {}
+            for key in FP_unaveraged_sel.keys():
+                if key in stamp_names:
+                    continue
+                else:
+                    FP_unaveraged_sel_fil.update({key: FP_unaveraged_sel[key]})
+            FP_unaveraged_sel = FP_unaveraged_sel_fil
+        if options['psfex']:
+            PSFEx_unaveraged_sel_fil = {}
+            for key in PSFEx_unaveraged_sel.keys():
+                if key in stamp_names:
+                    continue
+                else:
+                    PSFEx_unaveraged_sel_fil.update({key: PSFEx_unaveraged_sel[key]})
+            PSFEx_unaveraged_sel = PSFEx_unaveraged_sel_fil
+        if options['fit_analytic']:
+            WavefrontPSF_analytic_unaveraged_sel_fil = {}
+            for key in WavefrontPSF_analytic_unaveraged_sel.keys():
+                if key in stamp_names:
+                    continue
+                else:
+                    WavefrontPSF_analytic_unaveraged_sel_fil.update({key: WavefrontPSF_analytic_unaveraged_sel[key]})
+            WavefrontPSF_analytic_unaveraged_sel = WavefrontPSF_analytic_unaveraged_sel_fil
+        if options['fit']:
+            WavefrontPSF_unaveraged_sel_fil = {}
+            for key in WavefrontPSF_unaveraged_sel.keys():
+                if key in stamp_names:
+                    continue
+                else:
+                    WavefrontPSF_unaveraged_sel_fil.update({key: WavefrontPSF_unaveraged_sel[key]})
+            WavefrontPSF_unaveraged_sel = WavefrontPSF_unaveraged_sel_fil
 
 
 
@@ -217,22 +261,31 @@ if stamp_save == 0:
 # save catalogs
 ##############################################################################
 
-np.save(output_directory + 'validation_data',
-        FP_unaveraged)
-np.save(output_directory + 'validation_psfex',
-        PSFEx_unaveraged)
-np.save(output_directory + 'validation_fit',
-        WavefrontPSF_unaveraged)
-np.save(output_directory + 'validation_fit_analytic',
-        WavefrontPSF_analytic_unaveraged)
+if options['data']:
+    np.save(output_directory + 'validation_data',
+            FP_unaveraged)
+if options['psfex']:
+    np.save(output_directory + 'validation_psfex',
+            PSFEx_unaveraged)
+if options['fit']:
+    np.save(output_directory + 'validation_fit',
+            WavefrontPSF_unaveraged)
+if options['fit_analytic']:
+    np.save(output_directory + 'validation_fit_analytic',
+            WavefrontPSF_analytic_unaveraged)
 
-np.save(output_directory + 'trained_data',
-        FP_unaveraged_sel)
-np.save(output_directory + 'trained_psfex',
-        PSFEx_unaveraged_sel)
-np.save(output_directory + 'trained_fit',
-        WavefrontPSF_unaveraged_sel)
-np.save(output_directory + 'trained_fit_analytic',
-        WavefrontPSF_analytic_unaveraged_sel)
+if options['training']:
+    if options['data']:
+        np.save(output_directory + 'trained_data',
+                FP_unaveraged_sel)
+    if options['psfex']:
+        np.save(output_directory + 'trained_psfex',
+                PSFEx_unaveraged_sel)
+    if options['fit']:
+        np.save(output_directory + 'trained_fit',
+                WavefrontPSF_unaveraged_sel)
+    if options['fit_analytic']:
+        np.save(output_directory + 'trained_fit_analytic',
+                WavefrontPSF_analytic_unaveraged_sel)
 
 
